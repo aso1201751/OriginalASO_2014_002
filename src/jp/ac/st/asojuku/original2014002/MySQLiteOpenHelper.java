918f55e34c0799923f366870ff0b8d8125cccb73
aso1201751 <1201751@st.asojuku.ac.jp>
2014-06-09 16:30:50 +0900

    SQLの中編まで

--- src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -5,6 +5,18 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 
+	/**
+	 *  @param context 呼び出しコンテクスト
+	 *  @param name 利用DB名
+	 *  @param factory カーソルファクトリー
+	 *  @param version DBバージョン
+	 */
+	public MySQLiteOpenHelper(Context context){
+
+		super(context,"20140021201751.sqlite3",null,1);
+		}
+	}
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
@@ -14,6 +26,8 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO 自動生成されたメソッド・スタブ
+		db.execSQL("drop table Hitokoto");
+		onCreate(db);
 
 	}
 
