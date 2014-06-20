9f68e08627e4bf9c59394ee0ba31aa38d95292a7
aso1201751 <1201751@st.asojuku.ac.jp>
2014-06-20 11:13:09 +0900

    デバッグするとこ

--- src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MaintenanceActivity.java
@@ -1,15 +1,17 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class MaintenanceActivity extends Activity implements
 View.OnClickListener, AdapterView.OnItemClickListener
@@ -63,21 +65,62 @@ View.OnClickListener, AdapterView.OnItemClickListener
 		cursor = this.helper.selectHitokotoList(sdb);
 
 		int db_layout = android.R.layout.simple_list_item_1;
+		String[]from = {"phrase"};
+		int[] to = new int[]{android.R.id.text1};
+
+		SimpleCursorAdapter adapter =
+				new SimpleCursorAdapter(this,db_layout,cursor,from,to,0);
+
+		lsthtkt.setAdapter(adapter);
 	}
+
+	private void deleteFromHitokoto(int id){
+		if(sdb == null){
+			helper = new MySQLiteOpenHelper(getApplicationContext());
+		}
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			Log.e("ERROR",e.toString());
+		}finally{
+			this.helper.deleteHitokoto(sdb,id);
+		}
+	}
+
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 		// TODO 自動生成されたメソッド・スタブ
+		if(this.selectedID!=-1){
+			parent.getChildAt(this.lastPosition).setBackgroundColor(0);
+		}
+		view.setBackgroundColor(android.graphics.Color.LTGRAY);
 
+		SQLiteCursor cursor = (SQLiteCursor)parent.getItemAtPosition(position);
+		this.selectedID = cursor.getInt(cursor.getColumnIndex("_id"));
+		this.lastPosition = position;
 	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
 		switch(v.getId()){
+		case R.id.btndlt:
+			if(this.selectedID != -1){
+				this.deleteFromHitokoto(this.selectedID);
+				ListView lsthtkt = (ListView)findViewById(R.id.lsthtkt);
+				this.setDBValuetoList(lsthtkt);
+				this.selectedID = -1;
+				this.lastPosition = -1;
+			}
+			else{
+				Toast.makeText(MaintenanceActivity.this, "削除する行を選んでください", Toast.LENGTH_SHORT).show();
+			}
+			break;
+
 		case R.id.btnmdr:
-			Intent intent3 = new Intent(MaintenanceActivity.this,MainActivity.class);
-			startActivity(intent3);
+			 finish();
+			 break;
 		}
 
 	}
--- src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -18,15 +18,15 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 	 */
 	public MySQLiteOpenHelper(Context context){
 
-		super(context,"20140021201751.sqlite3",null,1);
+		super(context,"20140021201751.sqlite3",null,3);
 
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
-		db.execSQL("CREATE TABLE IF NOT EXISTS"+
-				"Hiokoto(_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,phrase TEXT)");
+		db.execSQL("CREATE TABLE IF NOT EXISTS "+
+				"Hitokoto(_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,phrase TEXT)");
 	}
 
 	@Override
